39ab374d8a2b34573019ecc969ed0589402db77a
eelishh <elisapierini9@gmail.com>
2023-01-06 23:55:16 +0100

    Update CommandLine.sh

--- CommandLine.sh
+++ CommandLine.sh
@@ -11,4 +11,4 @@ cut -d, -f 1 edges.csv | sort | uniq -c | sort -nr| head -n 10
 echo " "
 
 echo "The average number of heroes in comics:"
-cut -d, -f 1,2 edges.csv | awk -F, '{heroes[$2]++} END{for(i in heroes){t+= heroes[i]; print i, t/length(heroes)}}' | head -n 10
+awk -F',' '{heroes_per_comic[$1]++; total_heroes++} END{for (comic in heroes_per_comic) num_comics++; mean=total_heroes/num_comics; printf "average number of heroes in comic: %d\n", mean}' edges.csv
